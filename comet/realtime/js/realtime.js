4a019c179460298b92b279600ad0a61a09ee16df
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-06-05 20:14:50 +0300

    Add files via upload

---
+++ comet/realtime/js/realtime.js
@@ -0,0 +1,24 @@
+const ctx = document.getElementById('chart').getContext('2d');
+const realtime = new Chart(ctx).Bar({
+  labels: [],
+  datasets: [{
+    fillColor: 'rgba(0,60,100,1)',
+    strokeColor: 'black',
+    data: []
+  }]
+}, {
+  responsive: true,
+  barValueSpacing: 2
+});
+
+let isFirst = true;
+const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
+
+ws.addEventListener('message', event => {
+  if(isFirst) {
+    JSON.parse(event.data).forEach(data => realtime.addData([Number(data.online)], data.time));
+    isFirst = false;
+  }else {
+    realtime.addData([Number(JSON.parse(event.data).online)], JSON.parse(event.data).time);
+  }
+});
